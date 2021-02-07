334fd445fdc076e06d4e66500603f96fb21201cb
Sukanya <sukanya@unknown.home>
2021-02-07 12:42:15 +0000

    google automation script
    Signed-off-by: Sukanya <sukanya@unknown.home>

---
+++ Drupal/src/introduction/google.java
@@ -0,0 +1,22 @@
+package introduction;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class google {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver","/Users/sukanya/Desktop/chromedriver33");
+		WebDriver Driver = new ChromeDriver();
+		Driver.get("https://www.google.com");
+		Driver.switchTo().frame(Driver.findElement(By.xpath("/html/body/div/div[4]/div[3]/div/div[2]/span/div/div/iframe")));
+		Driver.findElement(By.xpath("/html/body/div/c-wiz/div[2]/div/div/div/div/div[2]/form/div/span/span")).click();
+		Driver.findElement(By.xpath("/html/body/div/div[2]/form/div[2]/div[1]/div[1]/div/div[2]/input")).sendKeys("performance testing");
+		Driver.findElement(By.xpath("/html/body/div/div[2]/form/div[2]/div[1]/div[1]/div/div[2]/input")).submit();
+		
+        
+	}
+
+}
